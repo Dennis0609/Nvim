bc4547956ac42c24a8100fc8652070767c5f2c27
Dennis0609 <liaojingneng@163.com>
2026-01-30 14:25:07 +0800

    New update

--- lua/plugins/pairs.lua
+++ lua/plugins/pairs.lua
@@ -1,18 +1,19 @@
 return {
-  "folke/which-key.nvim",
+  "nvim-mini/mini.pairs",
   event = "VeryLazy",
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    modes = { insert = true, command = true, terminal = false },
+    -- skip autopair when next character is one of these
+    skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+    -- skip autopair when the cursor is inside these treesitter nodes
+    skip_ts = { "string" },
+    -- skip autopair when next character is closing pair
+    -- and there are more closing pairs than opening pairs
+    skip_unbalanced = true,
+    -- better deal with markdown code blocks
+    markdown = true,
   },
-  keys = {
-    {
-      "<leader>?",
-      function()
-        require("which-key").show({ global = false })
-      end,
-      desc = "Buffer Local Keymaps (which-key)",
-    },
-  },
-} -- Take into account that `virtualedit=all` can go into inline virtual text
+  config = function(_, opts)
+    LazyVim.mini.pairs(opts)
+  end,
+}
